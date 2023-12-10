d301bf8f777a045578ae0150888a8fd177964e79
vladsokol73 <73sokol73@list.ru>
2023-12-10 12:19:55 +0400

    favicon

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="/css/contact.css">
     <link rel="stylesheet" href="/css/header.css">
     <link rel="stylesheet" href="/css/footer.css">
+    <link rel="shortcut icon" href="{{ url("/storage/images/icon.png") }}">
     <script src="/js/app.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
           integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
