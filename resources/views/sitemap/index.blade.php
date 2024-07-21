fd70bd5188d5f669c61826184b5c2e9cdb1d3ad6
vladsokol73 <73sokol73@list.ru>
2024-07-21 12:02:31 +0400

    seo

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,15 +1,6 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>https://plc-pro100.ru</loc>
-    </url>
-    <url>
-        <loc>https://plc-pro100.ru/catalog</loc>
-    </url>
-    <url>
-        <loc>https://plc-pro100.ru/brands</loc>
-    </url>
     <sitemap>
         <loc>{{ url('sitemap/products') }}</loc>
         <lastmod>{{ $product->updated_at->toAtomString() }}</lastmod>
@@ -19,3 +10,15 @@
         <lastmod>{{ $category->updated_at->toAtomString() }}</lastmod>
     </sitemap>
 </sitemapindex>
+
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url>
+    <loc>https://plc-pro100.ru</loc>
+</url>
+<url>
+    <loc>https://plc-pro100.ru/catalog</loc>
+</url>
+<url>
+    <loc>https://plc-pro100.ru/brands</loc>
+</url>
+</urlset>
