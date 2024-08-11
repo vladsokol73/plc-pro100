1e0be0e49329587ca5fbe81585b4963f2a2d55e0
vladsokol73 <73sokol73@list.ru>
2024-08-11 10:35:37 +0400

    feed

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <p class="footer-title">О компании</p>
     <p class="info">PLC-Pro не является уполномоченным дистрибьютором или представителем производителя. Размещенные на этом сайте торговые марки являются собственностью их соответствующих владельцев</p>
-    <p class="info">Наша почта - <a href="mailto:burdin100@mail.ru">burdin100@mail.ru</a><a href="mailto:burdin100@outlook.com">burdin100@outlook.com</a></p>
+    <p class="info">Наша почта - <a href="mailto:burdin100@mail.ru">burdin100@mail.ru</a> <a href="mailto:burdin100@outlook.com">burdin100@outlook.com</a></p>
     <p class="info">Телефон <a href="tel:+79297949431">+7(929)794-94-31</a></p>
     <div class="footer-social-container">
         <div class="left">
