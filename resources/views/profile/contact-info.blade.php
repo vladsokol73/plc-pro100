f8db4426d3fd81ed938c513b67887c188af0ea18
vladsokol73 <73sokol73@list.ru>
2025-06-04 13:52:33 +0400

    fix contact

--- resources/views/profile/contact-info.blade.php
+++ resources/views/profile/contact-info.blade.php
@@ -63,7 +63,6 @@
                                    type="checkbox"
                                    id="is_active"
                                    name="is_active"
-                                   value="1"
                                    @if(isset($contactInfo) && $contactInfo->is_active) checked @endif>
                             <label class="form-check-label" for="is_active">
                                 Активно
